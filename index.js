b3830df2fba012dc376380190a4c69458f69633e
Oybek Toshmatov <m.oybek9508@gmail.com>
2021-06-07 18:35:30 +0900

    First commit

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ require('./startup/routes')(app)
 require('./startup/database')()
 require('./startup/config')
 require('./startup/validation')()
-require('./startup/prod')(app)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
